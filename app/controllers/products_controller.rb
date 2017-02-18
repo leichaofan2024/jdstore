ee9d33429c226454336b4966bb289c02f91c7b4f
leichaofan <leichaofan2024@gmail.com>
2017-02-18 22:03:19 +0800

    mogai welcome and search 2

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,5 +1,6 @@
 class ProductsController < ApplicationController
   before_action :current_user_required
+  before_action :validate_search_key, only: [:search]
   def index
     @product = Product.all
   end
@@ -16,6 +17,27 @@ class ProductsController < ApplicationController
   end
     redirect_to :back
   end
+  # 下面是搜索功能代码
+  def search
+    if @query_string.present?
+      search_result = Product.ransack(@search_criteria).result(:distinct => true)
+      @products = search_result.paginate(:page => params[:page], :per_page => 5 )
+    end
+  end
+
+
+  protected
+
+  def validate_search_key
+    @query_string = params[:q].gsub(/\\|\'|\/|\?/, "") if params[:q].present?
+    @search_criteria = search_criteria(@query_string)
+  end
+
+
+  def search_criteria(query_string)
+    { :title_cont => query_string }
+  end
+  # 以上搜索功能代码
   private
   def product_params
     params.require(:product).permit(:title, :description, :quantity, :price)
